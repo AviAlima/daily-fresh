11c028e731a0eb54ee9ed3b97cb2b2edf6712e75
Avi Alima <avi.alima@gmail.com>
2026-08-08 00:52:36 +0300

    Bump version to v15

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
   var OLD_KEY = 'daily-fresh-state';
   var BACKUP_KEYS = ['daily-fresh-state-b1', 'daily-fresh-state-b2', 'daily-fresh-state-b3'];
   var CORRUPT_KEY = 'daily-fresh-state-corrupt';
-  var APP_VERSION = 'v14';
+  var APP_VERSION = 'v15';
 
   var state = load();
   var activeDay = state.activeDay || currentDayKey();
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'daily-fresh-v14';
+var CACHE = 'daily-fresh-v15';
 
 var ASSETS = [
   './',
